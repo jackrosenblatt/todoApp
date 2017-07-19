f2aaa272e96b3563e9544e85de92f7f14ea8e71a
Jack <jdrosenblatt18@gmail.com>
2017-07-19 15:50:18 -0700

    input line functionality

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -3,15 +3,32 @@ import React from 'react';
 class InputLine extends React.Component{
   constructor(props){
     super(props)
+    this.state = {
+      typedText: ''
+    }
+  }
+
+  handleTypeing(e){
+    this.setState({typedText: e.target.value})
+  }
+
+  handleSubmit(){
+    this.props.submit(this.state.typedText);
+    this.setState({typedText: ''})
+  }
+
+  handleFormSubmit(e){
+    e.preventDefault();
+    this.handleSubmit()
   }
 
   render(){
     return (
-      <form>
+      <form onSubmit={(e) => this.handleFormSubmit(e)}>
         <div className='form-group'>
           <div className='input-group'>
-            <input type='text'></input>
-            <div className='btn btn-primary addButton' onClick={() => this.props.submit('Test Task')} type='submit'>Add todo</div>
+            <input type='text' value={this.state.typedText} onChange={(e) => this.handleTypeing(e)}></input>
+            <div className='btn btn-primary addButton' onClick={() => this.handleSubmit()} type='submit'>Add todo</div>
           </div>
         </div>
       </form>
